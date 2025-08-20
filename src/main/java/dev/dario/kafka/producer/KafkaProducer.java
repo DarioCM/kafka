b86ff924583b9463e3242e02b4f556f03c57e989
Carlos Castañeda <dario20049@gmail.com>
2025-08-21 01:44:59 +0900

    feat: add kafka producer for order messages

---
+++ src/main/java/dev/dario/kafka/producer/KafkaProducer.java
@@ -0,0 +1,24 @@
+package dev.dario.kafka.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+  private KafkaTemplate<String, String> kafkaTemplate;
+
+  public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendOrderMessage(String order) {
+    LOGGER.info(String.format("Order sent %s", order));
+    kafkaTemplate.send("orders", order);
+  }
+
+
+}
