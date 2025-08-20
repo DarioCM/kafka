ef0258df1fde3458c70df54b9bbf6946e7495d4f
Carlos Castañeda <dario20049@gmail.com>
2025-08-20 17:05:57 +0900

    Initial commit

---
+++ src/test/java/dev/dario/kafka/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dario.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
