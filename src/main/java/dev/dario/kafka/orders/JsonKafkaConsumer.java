f4dd2be2ac45c21503a3cb083ec66ef274a8996b
Carlos Castañeda <dario20049@gmail.com>
2025-08-22 21:15:10 +0900

    feat: JsonKafkaConsumer

---
+++ src/main/java/dev/dario/kafka/orders/JsonKafkaConsumer.java
@@ -0,0 +1,19 @@
+package dev.dario.kafka.orders;
+
+import dev.dario.kafka.orders.dto.OrderEventDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+
+  @KafkaListener(topics = "ordersJSON", groupId = "consumer-group")
+  public void consume(OrderEventDTO order) {
+    LOGGER.info(String.format("JSON Order message recieved -> %s ", order.toString()));
+  }
+
+}
